1ce5e7a13566da19b8079e3061fc94fa97a954ec
Nikita Serov <nikita-serov2003@yandex.ru>
2024-10-28 16:40:21 +0500

    added text output

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
+if len(faces) > 0:
+    print(f"Found {len(faces)} faces:")
+    for i, (x, y, w, h) in enumerate(faces):
+        print(f"Face {i + 1}: x={x}, y={y}, w={w}, h={h}")
+else:
+    print("No faces found.")
+
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
